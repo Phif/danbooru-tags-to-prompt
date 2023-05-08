e5e2a795cdde3776633a78e30414a2e0f26b2f46
Phif <71464584+Phif@users.noreply.github.com>
2023-05-08 03:54:27 +0200

    added deepbooru support

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ const inputPaste = document.getElementById("input-paste");
 const outputCopy = document.getElementById("output-copy");
 let tags; 
 
+const inputArea2 = document.getElementById("input2");
+const outputArea2 = document.getElementById("output2");
+const inputPaste2 = document.getElementById("input-paste2");
+const outputCopy2 = document.getElementById("output-copy2");
+let tags2; 
+
 inputArea.oninput = () => {
     printToOutputArea();
 }
@@ -30,4 +36,25 @@ function printToOutputArea() {
     tags = tags.split("\n")
 
     outputArea.value = tags.join(", ");
+}
+
+inputArea2.oninput = () => {
+    printToOutputArea2();
+}
+
+inputPaste2.onclick = async () => {
+    if (navigator.clipboard) {
+        inputArea2.value = await navigator.clipboard.readText();
+        printToOutputArea();
+    }
+}
+
+outputCopy2.onclick = () => {
+    navigator.clipboard.writeText(outputArea2.value)
+}
+
+function printToOutputArea2() {
+    tags2 = inputArea2.value;
+    tags2 = tags2.replaceAll("_", " ")
+    outputArea2.value = tags2;
 }
