02a36c742c53249848b2117ae61e589e8c699abc
Phif <philippesilva.94@gmail.com>
2024-12-30 18:20:27 +0100

    feat: converter v2

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'material-design-icons/iconfont/material-icons.css';
 
 const app = createApp(App)
 
