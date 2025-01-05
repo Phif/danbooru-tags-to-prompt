aee79a163c963ddf03b05598d8e3024172a4b5a6
Phif <philippesilva.94@gmail.com>
2025-01-05 15:47:43 +0100

    add gh pages config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/danbooru-tags-to-prompt/',
   plugins: [
     vue(),
     vueDevTools(),
